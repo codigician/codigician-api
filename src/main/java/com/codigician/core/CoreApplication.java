be6ffeccd0fcc38294f4d2ac121870e78df82554
bilginyuksel <bilgin.yuksel96@gmail.com>
2021-10-13 23:44:52 +0300

    Updated application properties port default value and health check api

--- src/main/java/com/codigician/core/CoreApplication.java
+++ src/main/java/com/codigician/core/CoreApplication.java
@@ -3,11 +3,19 @@ package com.codigician.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
+@RestController
 public class CoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
 	}
 
+    @GetMapping("/health")
+    public void health() {
+        System.out.println("health check");
+    }
 }
